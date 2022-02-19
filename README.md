1f3ca6661a5b78953c703df89ea02143a168594c
configtheworld <erkinture@gmail.com>
2022-02-19 19:45:43 +0300

    open, list, add added

--- README.md
+++ README.md
@@ -1,7 +1,32 @@
 # desktop-browser-automation
 
-Little python script for open multiple tabs and play the youtube video you want to start your day. Its customizable with default urls and command-line options.
+Little python script for open multiple tabs and play the youtube musics you want to start your day. Its customizable with default urls and command-line options.
 
 ### Usage
 
-`$ pip install selenium`
+```bash
+python3 index.py -a <url>
+# > => Added to list: <url>
+
+python3 index.py -l
+# > -Your URL List-
+# 1 => *open*  <url>
+# 2 => *open*  <url2>
+# 3 => *open*  <url3>
+
+python3 index.py -o
+#> => Here is your favorite Tabs
+```
+
+### Arguments
+
+- add
+  - -a or --add <url>
+- remove
+  - -r or --remove <index> (index visible form list)
+- open
+  - -o or --open
+- play
+  - -p or --play
+- list
+  - -l or --list
--- index.py
+++ index.py
@@ -1,61 +1,101 @@
-from selenium.webdriver import Firefox
-from selenium.webdriver.firefox.options import Options
-from time import sleep, ctime
-from collections import namedtuple
-from threading import Thread
-from os.path import isfile
+import webbrowser
+import getopt
+import sys
 import csv
-import aaargh
 
 
-app = aaargh.App(description="desktop-browser-automation")
+# csv file name
+filename = "urls.csv"
 
-# Application level arguments:
-app.arg('--name', help="Name of the person to greet", default="stranger")
+# initializing the titles and rows list
+fields = ["Index", "Url", "Operation"]
+rows = []
 
-# Application level defaults:
-app.defaults(name="visitor")  # overrides "stranger"
+# reading csv file
+with open(filename, 'r') as csvfile:
+    # creating a csv reader object
+    csvreader = csv.reader(csvfile)
 
+    # extracting field names through first row
+    fields = next(csvreader)
 
-@app.cmd
-def hello(name):  # application level "name" argument is always passed
-    print("Hello, world!")
+    # extracting each data row one by one
+    for row in csvreader:
+        rows.append(row)
 
+# create argument list
+argumentList = sys.argv[1:]
 
-@app.cmd(name="hi", help="Say hi")  # override subcommand name
-@app.cmd_arg('-r', '--repeat', type=int, default=1, help="How many times?")
-def say_hi(name, repeat):  # both application and subcommand args
-    for i in range(repeat):
-        print("Hi, %s!" % name)
-
-
-@app.cmd
-# overrides "visitor" for this command only
-@app.cmd_defaults(name="my friend")
-def greetings(name):
-    print("Greetings, %s." % name)
-
-
-@app.cmd(alias='bye')  # Allow "bye" aswell as goodbye
-def goodbye(name):
-    print("Goodbye, cruel world!")
+# Options
+options = "a:r:op:l"
+# Long options
+long_options = ["add", "remove", "open", "play", "list"]
 
 
 class BrowserAutomation ():
     def __init__(self):
-        # Create a headless browser
-        opts = Options()
-        opts.add_argument("--headless")
-        self.browser = Firefox(options=opts)
-        self.browser.get("https://github.com/configtheworld")
+        # no need init
+        pass
 
-        # States
-        self.open = []
-        self.play = []
-        self.start()
+    def add(self, url):
+        with open(filename, 'a') as csvfile:
+            cvswriter = csv.writer(csvfile)
+            cvswriter.writerows(
+                [[len(rows)+1, url, "o"]])
+            print("=> Added to list:", url)
 
-    def start(self):
-        app.run()
+    def remove(self, number):
+        # with open(filename, 'w') as csvfile:
+        #     cvswriter = csv.DictWriter(csvfile, fieldnames=fields)
+        #     rows.remove(int(number))
+        #     cvswriter.writeheader()
+        #     cvswriter.writerows(rows)
+
+        print(number, "=> Not Removed from list Yet")
+
+    def open(self):
+        print(currentValue, "=> Here is your favorite Tabs")
+        for row in rows:
+            if row[2] == "o":
+                webbrowser.open_new_tab(row[1])
+
+    def play(self):
+        print(currentValue, "=> Playing")
+        # TODO
+
+    def show_list(self):
+        print("-Your URL List-")
+        for row in rows:
+            print(row[0], "=>",  "*open* " if row[2]
+                  == "o" else "*play* ", row[1])
 
 
 ba = BrowserAutomation()
+
+
+try:
+    # Parsing argument
+    arguments, values = getopt.getopt(argumentList, options, long_options)
+
+    # checking each argument
+    for currentArgument, currentValue in arguments:
+
+        if currentArgument in ("-a", "--add"):
+            # add url to csv
+            ba.add(currentValue)
+        elif currentArgument in ("-r", "--remove"):
+            # remove url from csv
+            ba.remove(currentValue)
+        elif currentArgument in ("-o", "--open"):
+            # open tabs from csv
+            ba.open()
+        elif currentArgument in ("-p", "--play"):
+            # play url
+            ba.play()
+        elif currentArgument in ("-l", "--list"):
+            # show csv list
+            ba.show_list()
+
+except getopt.error as err:
+    # output error, and return with an error code
+    print(str(err))
